0171a480e2ef1c1f442cd2236112cacb8352857a
a dinosaur <dino@a-dinosaur.com>
2025-06-25 05:40:21 +1000

    c: Lesson13 - Use packed quad API instead of baked API

--- src/c/lesson13.c
+++ src/c/lesson13.c
@@ -13,45 +13,81 @@
 
 
 static SDL_GPUTexture* fontTex = NULL;
-static stbtt_bakedchar glyphs[96];
+static stbtt_packedchar fontChars[96];
 
 static float projection[16];
 
 static float counter1 = 0.0f, counter2 = 0.0f;
 
 
+static bool NeHe_BuildFont(NeHeContext* restrict ctx, const char* const restrict ttfResourcePath,
+	int fontSize)
+{
+	void* ttf = NeHe_ReadResourceBlob(ctx, ttfResourcePath, NULL);
+	if (!ttf)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to read font file: %s", SDL_GetError());
+		return false;
+	}
+
+	uint8_t pixels[FONT_ATLAS_W * FONT_ATLAS_H];
+
+	//FW_BOLD is 700
+	stbtt_pack_context packCtx;
+	int res = stbtt_PackBegin(&packCtx, pixels, FONT_ATLAS_W, FONT_ATLAS_H, 0, 1, NULL);
+	if (!res) { return false; }
+	res = stbtt_PackFontRange(&packCtx, ttf, 0, (float)fontSize, ' ', 96, fontChars);
+	if (!res) { return false; }
+	SDL_free(ttf);
+	stbtt_PackEnd(&packCtx);
+
+	const SDL_GPUTextureCreateInfo fontInfo =
+	{
+		.type = SDL_GPU_TEXTURETYPE_2D,
+		.format = SDL_GPU_TEXTUREFORMAT_R8_UNORM,  //SDL_GPU_TEXTUREFORMAT_A8_UNORM
+		.usage = SDL_GPU_TEXTUREUSAGE_SAMPLER,
+		.width = FONT_ATLAS_W, .height = FONT_ATLAS_H, .layer_count_or_depth = 1,
+		.num_levels = 1,
+		.sample_count = SDL_GPU_SAMPLECOUNT_1,
+		.props = 0
+	};
+	if ((fontTex = NeHe_CreateGPUTextureFromPixels(ctx, pixels, sizeof(pixels), &fontInfo, false)) == NULL)
+	{
+		return false;
+	}
+
+	return true;
+}
+
 static void NeHe_DrawText(SDL_GPUCommandBuffer* restrict cmd, SDL_GPUTexture* restrict swapchain, float x, float y, const unsigned char* restrict text)
 {
 	for (int c = *text; c != '\0'; c = (int)*(++text))
 	{
 		stbtt_aligned_quad quad;
-		stbtt_GetBakedQuad(glyphs, FONT_ATLAS_W, FONT_ATLAS_H, c - 0x20, &x, &y, &quad, 1);
+		stbtt_GetPackedQuad(fontChars, FONT_ATLAS_W, FONT_ATLAS_H, c - 0x20, &x, &y, &quad, 1);
 
 		if (c < 0x20 || c > 0x7F)
 			continue;
 
-		if (quad.s0 == quad.s1 || quad.t0 == quad.t1 || quad.x0 == quad.x1 || quad.y0 == quad.y1)
+		unsigned srcX = (unsigned)(quad.s0 * FONT_ATLAS_W);
+		unsigned srcY = (unsigned)(quad.t0 * FONT_ATLAS_H);
+		unsigned srcW = (unsigned)((quad.s1 - quad.s0) * FONT_ATLAS_W);
+		unsigned srcH = (unsigned)((quad.t1 - quad.t0) * FONT_ATLAS_H);
+		if (!srcW || !srcH)
 			continue;
 
-		const float scale = 1.0f;
+		const float scale = 2.0f;
+		unsigned dstX = (unsigned)(scale * (quad.x0));
+		unsigned dstY = (unsigned)(scale * (quad.y0));
+		unsigned dstW = (unsigned)(scale * (quad.x1 - quad.x0));
+		unsigned dstH = (unsigned)(scale * (quad.y1 - quad.y0));
+		if (!dstW || !dstH)
+			continue;
+
 		const SDL_GPUBlitInfo blitInfo =
 		{
-			.source =
-			{
-				.texture = fontTex,
-				.x = (unsigned)(quad.s0 * FONT_ATLAS_W),
-				.y = (unsigned)(quad.t0 * FONT_ATLAS_H),
-				.w = (unsigned)((quad.s1 - quad.s0) * FONT_ATLAS_W),
-				.h = (unsigned)((quad.t1 - quad.t0) * FONT_ATLAS_H)
-			},
-			.destination =
-			{
-				.texture = swapchain,
-				.x = (unsigned)(scale * (quad.x0)),
-				.y = (unsigned)(scale * (quad.y0)),
-				.w = (unsigned)(scale * (quad.x1 - quad.x0)),
-				.h = (unsigned)(scale * (quad.y1 - quad.y0))
-			},
+			.source = { .texture = fontTex, .x = srcX, .y = srcY, .w = srcW, .h = srcH },
+			.destination = { .texture = swapchain, .x = dstX, .y = dstY, .w = dstW, .h = dstH },
 			.load_op = SDL_GPU_LOADOP_LOAD,
 			.filter = SDL_GPU_FILTER_LINEAR
 		};
@@ -73,30 +109,7 @@ static void NeHe_Printf(SDL_GPUCommandBuffer* restrict cmd, SDL_GPUTexture* rest
 
 static bool Lesson13_Init(NeHeContext* ctx)
 {
-	void* ttf = NeHe_ReadResourceBlob(ctx, "Data/NimbusMonoPS-Bold.ttf", NULL);
-	if (!ttf)
-	{
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to read font file: %s", SDL_GetError());
-		return false;
-	}
-
-	uint8_t pixels[FONT_ATLAS_W * FONT_ATLAS_H];
-
-	//FW_BOLD is 700
-	stbtt_BakeFontBitmap(ttf, 0, 24, pixels, FONT_ATLAS_W, FONT_ATLAS_H, ' ', 96, glyphs);
-	SDL_free(ttf);
-
-	const SDL_GPUTextureCreateInfo fontInfo =
-	{
-		.type = SDL_GPU_TEXTURETYPE_2D,
-		.format = SDL_GPU_TEXTUREFORMAT_R8_UNORM,  //SDL_GPU_TEXTUREFORMAT_A8_UNORM
-		.usage = SDL_GPU_TEXTUREUSAGE_SAMPLER,
-		.width = FONT_ATLAS_W, .height = FONT_ATLAS_H, .layer_count_or_depth = 1,
-		.num_levels = 1,
-		.sample_count = SDL_GPU_SAMPLECOUNT_1,
-		.props = 0
-	};
-	if ((fontTex = NeHe_CreateGPUTextureFromPixels(ctx, pixels, sizeof(pixels), &fontInfo, false)) == NULL)
+	if (!NeHe_BuildFont(ctx, "Data/NimbusMonoPS-Bold.ttf", 24))
 	{
 		return false;
 	}
@@ -146,6 +159,26 @@ static void Lesson13_Draw(NeHeContext* restrict ctx, SDL_GPUCommandBuffer* restr
 	float rasterY = 0.35f * SDL_sinf(counter2);
 
 	SDL_EndGPURenderPass(pass);
+
+	const SDL_GPUBlitInfo blitInfo =
+	{
+		.source =
+		{
+			.texture = fontTex,
+			.w = (unsigned)FONT_ATLAS_W,
+			.h = (unsigned)FONT_ATLAS_H
+		},
+		.destination =
+		{
+			.texture = swapchain,
+			.y = 50u,
+			.w = (unsigned)FONT_ATLAS_W * 3u,
+			.h = (unsigned)FONT_ATLAS_H * 3u
+		},
+		.load_op = SDL_GPU_LOADOP_LOAD,
+		.filter = SDL_GPU_FILTER_NEAREST
+	};
+	SDL_BlitGPUTexture(cmd, &blitInfo);
 
 	rasterX += 10.0f;
 	rasterY += 20.0f;
