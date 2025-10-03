e7b79f2d142ce4fe5a3eebc576c97bcf41bb5490
a dinosaur <dino@a-dinosaur.com>
2025-10-03 16:45:03 +1000

    shaders: Implement HLSL for Lesson20

---
+++ src/shaders/lesson20.hlsl
@@ -0,0 +1,54 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2025 a dinosaur
+ * SPDX-License-Identifier: Zlib
+ */
+
+struct VertexUniform
+{
+	float4x4 modelViewProj;
+	float2 texOffset, texScale;
+};
+
+struct Vertex2Pixel
+{
+	float4 position : SV_Position;
+	float2 texCoord : TEXCOORD0;
+};
+
+ConstantBuffer<VertexUniform> ubo : register(b0, space1);
+
+static const float2 quadVertices[4] =
+{
+	{ -1.1f, -1.1f },  // Bottom left
+	{  1.1f, -1.1f },  // Bottom right
+	{ -1.1f,  1.1f },  // Top left
+	{  1.1f,  1.1f }   // Top right
+};
+
+static const float2 quadTexCoords[4] =
+{
+    { 0.0f, 0.0f },  // Bottom left
+    { 1.0f, 0.0f },  // Bottom right
+    { 0.0f, 1.0f },  // Top left
+    { 1.0f, 1.0f }   // Top right
+};
+
+Vertex2Pixel VertexMain(uint vertexID : SV_VertexID)
+{
+	Vertex2Pixel output;
+	output.position = mul(ubo.modelViewProj, float4(quadVertices[vertexID], 0.0, 1.0));
+	output.texCoord = ubo.texOffset + quadTexCoords[vertexID] * ubo.texScale;
+	return output;
+}
+
+Texture2D<half4> Texture : register(t0, space2);
+SamplerState Sampler : register(s0, space2);
+
+#ifdef VULKAN
+half4 FragmentMain(Vertex2Pixel input) : SV_Target0
+#else
+half4 PixelMain(Vertex2Pixel input) : SV_Target0
+#endif
+{
+	return Texture.Sample(Sampler, input.texCoord);
+}
